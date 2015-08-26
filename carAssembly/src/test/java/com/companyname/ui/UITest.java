5393b5a8326bb8ff8bcc5635f6d332b84b1a4f55
mustfa <md.sajidmustfa@gmail.com>
2015-08-26 17:15:44 +0530

    TEST

---
+++ carAssembly/src/test/java/com/companyname/ui/UITest.java
@@ -0,0 +1,12 @@
+package com.companyname.ui;
+
+import org.testng.annotations.Test;
+import com.companyname.ui.*;
+
+public class UITest {
+	Info obj=new Info();
+  @Test
+  public void testDate() {
+	  System.out.println("Test");
+  }
+}
